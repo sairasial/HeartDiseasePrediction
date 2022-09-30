f37bc404fa711f421316f2f39f06440eca00211e
sairasial <57367353+sairasial@users.noreply.github.com>
2022-09-30 16:01:58 +0500

    Add files via upload

---
+++ HeartDiseasePrediction/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def predict(request):
+    return render(request, 'predict.html')
+
+def result(request):
+    data = pd.read_csv(r"C:\Users\Hp\Downloads\FYP Prediction\heart.csv")
+    X = data.drop("target", axis=1)
+    Y = data['target']
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
+    model = RandomForestClassifier(2000)
+    model.fit(X_train, Y_train)
+    val1 = float(request.GET['n1'])
+    val2 = float(request.GET['n2'])
+    val3 = float(request.GET['n3'])
+    val4 = float(request.GET['n4'])
+    val5 = float(request.GET['n5'])
+    val6 = float(request.GET['n6'])
+    val7 = float(request.GET['n7'])
+    val8 = float(request.GET['n8'])
+    val9 = float(request.GET['n9'])
+    val10 = float(request.GET['n10'])
+    val11= float(request.GET['n11'])
+    val12= float(request.GET['n12'])
+    val13 = float(request.GET['n13'])
+
+    pred = model.predict([[val1,val2,val3,val4,val5,val6,val7,val8,val9,val10,val11,val12,val13]])
+
+    result1=''
+    if pred==[1]:
+        result1='POSITIVE'
+    else:
+        result1='NEGATIVE'
+    print(result1)
+    return render(request, 'result.html',{'result2':result1})
